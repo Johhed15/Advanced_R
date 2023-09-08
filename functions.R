1a6caaf4193f4b1074816f07fdda13d2a26b1b3a
Johhed15 <johannes_15_04@hotmail.com>
2023-09-08 10:25:50 +0200

    J

---
+++ functions.R
@@ -0,0 +1,14 @@
+
+
+# Assignment 1 
+# Euc
+
+euclidean <- function(x,y){
+  # checking the input 
+  stopifnot(is.integer(x) && length(x) == 1 && is.integer(y) && length(y) == 1)
+  
+  
+  
+  
+}
+
