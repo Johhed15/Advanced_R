44e56c5c343efc18bf0e41ee37dc454c8c2768cc
Johhed15 <johannes_15_04@hotmail.com>
2023-09-11 09:53:37 +0200

    J

--- Package.labb3/R/dijkstra.R
+++ Package.labb3/R/dijkstra.R
@@ -1,34 +1,5 @@
-
-
-# Assignment 1 
-# Euc
-
-euclidean <- function(x,y){
-  # checking the input 
-  stopifnot(is.integer(x) || is.numeric(x) && length(x) == 1 ,is.integer(y) || is.numeric(y) && length(y) == 1)
-  
-  while (y != 0) { # While y differ from 0
-    t  <- y
-    y <- x %% y  # X modulus y
-    x <- t
-  }
-  return(abs(x)) # returning x which now is the GCD
-}
-
-euclidean(100, 1000)
-
-euclidean(23141234, 13892347912)
-
-
-
-
-# assignment 2
-# This function have gotten  pseudocode
-# Code inspiration from https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm 
-
-
-
-dijkstra <- function(graph, init_node){
+dijkstra <-
+function(graph, init_node){
   # checking the input
   stopifnot(is.data.frame(graph), colnames(graph) %in% c('v1','v2','w'),
             is.numeric(init_node) && length(init_node) == 1,
@@ -62,17 +33,3 @@ dijkstra <- function(graph, init_node){
    }
   return(dist)   
 }
-
-wiki_graph <-
-  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-
-dijkstra(wiki_graph, 1)
-
-
-dijkstra(wiki_graph, 3)
-
-dijkstra(wiki_graph, 6)
-
-package.skeleton(name='Package.labb3')
