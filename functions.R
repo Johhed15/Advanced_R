419bd27777d493ed1b279300a56e67d971b71f96
Johhed15 <johannes_15_04@hotmail.com>
2023-09-11 11:28:29 +0200

    J

--- functions.R
+++ functions.R
@@ -15,10 +15,6 @@ euclidean <- function(x,y){
   return(abs(x)) # returning x which now is the GCD
 }
 
-euclidean(100, 1000)
-
-euclidean(23141234, 13892347912)
-
 
 
 
@@ -63,16 +59,7 @@ dijkstra <- function(graph, init_node){
   return(dist)   
 }
 
-wiki_graph <-
-  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-
-dijkstra(wiki_graph, 1)
-
-
-dijkstra(wiki_graph, 3)
-
-dijkstra(wiki_graph, 6)
 
 package.skeleton(name='Package.labb3')
+
+install.packages('roxygen2')
