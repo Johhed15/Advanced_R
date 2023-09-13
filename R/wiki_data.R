6395c03371a382023e5cee7ee2d7273ca0d2474c
Johhed15 <johannes_15_04@hotmail.com>
2023-09-13 16:17:51 +0200

    J

---
+++ R/wiki_data.R
@@ -0,0 +1,14 @@
+#' Graph data from wikipedia
+#'
+#' Dijkstra algorithm example from wikipedia
+#'
+#' @format ## `wiki_data`
+#' A data frame with 18 rows and 6 columns:
+#' \describe{
+#'   \item{v1}{nodes, number of each node depends on number of neighbours of the node}
+#'   \item{v2}{connection between the nodes in v1}
+#'   \item{w}{distance between each pair of node}
+#'   ...
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+"wiki_data"
